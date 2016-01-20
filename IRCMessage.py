b9a49f29e98b3ef51791e1e4924384d728ac865c
norang@lemon <pinethee@naver.com>
2016-01-20 17:29:10 +0000

    new parser

--- IRCMessage.py
+++ IRCMessage.py
@@ -1,5 +1,5 @@
-#-*- coding: utf-8 -*-
 import re
+
 
 class IRCMessage():
     msgType = None
@@ -9,19 +9,36 @@ class IRCMessage():
     target = None
 
     def __init__(self, origMessage):
-        parse = re.search(r':.*!~(\S+)\s+(\S+)\s+(.*?)\s+:?(.*)', origMessage)
+        parse = re.search('^(?:[:](\S+) )?(\S+)(?: (?!:)(.+?))?(?: [:](.+))?$', origMessage)
         if parse:
-            self.sender = parse.group(1)
             self.msgType = parse.group(2)
-            self.channel = parse.group(3)
-            self.msg = parse.group(4)
             if self.msgType == 'INVITE':
+                self.sender = parse.group(1)
+                self.target = parse.group(3)
                 self.channel = parse.group(4)
+            elif self.msgType == 'PRIVMSG':
+                self.sender = parse.group(1)
+                self.channel = parse.group(3)
+                self.msg = parse.group(4)
+            elif self.msgType == 'MODE':
+                self.sender = parse.group(1)
+                self.channel = parse.group(3).split(' ', maxsplit=1)[0]
+                self.msg = parse.group(3).split(' ', maxsplit=1)[1]
+            elif self.msgType == 'JOIN':
+                self.sender = parse.group(1)
+                self.channel = parse.group(4)
+            elif self.msgType == 'PING':
+                self.sender = parse.group(4)
         else:
             pass
 
     def __repr__(self):
         msg = self.msg
-        if msg:
-            msg = msg.encode('utf-8')
-        return '<IRCMessage : %s %s %s %s %s>' % (self.msgType, self.sender, self.channel, msg, self.target)
+        return '<IRCMessage : %s %s %s %s %s>' \
+               % (self.msgType, self.sender, self.channel, msg, self.target)
+
+    def isValid(self):
+        if self.msgType == None:
+            return False
+        else:
+            return True
