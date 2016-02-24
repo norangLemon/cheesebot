8d4d53dadbfa746f651b841228c35dd758b3fbcc
norang <norangLemon@users.noreply.github.com>
2016-02-24 20:57:47 +0900

    Delete setting.py

--- setting.py
+++
@@ -1,9 +0,0 @@
-HOST = "evans.uriirc.org"
-PORT = 16662
-UTF8 = "UTF-8"
-NICK = "치즈"
-ID = "norang_cheese"
-CHANNEL = "#norang"
-
-ID_BYB_BOT = 'BYBbot@115.68.27.121'
-ID_NORANG = "~pinethee@li384-180.members.linode.com"
